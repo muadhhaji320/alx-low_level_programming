2aed24de70a573775d3dd42734593b40064e912a
muadh haji <muadhhaji320@gmail.com>
2022-07-07 10:10:25 -0700

    hot fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * The isupper() function checks whether a character is an uppercase alphabet (A-Z) or not.
+ * _isupper - function that checks for uppercase character.
  * @c: int type number
  * Return: 1 if c is uppercase, 0 otherwise
  */
