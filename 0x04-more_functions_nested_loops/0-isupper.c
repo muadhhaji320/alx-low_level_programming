fd96e1d6d385ebbad9c0390fb6a4ad616996d6a0
muadh haji <muadhhaji320@gmail.com>
2022-07-07 09:50:15 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * The isupper() function checks whether a character is an uppercase alphabet (A-Z) or not.
+ * @c: int type number
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
