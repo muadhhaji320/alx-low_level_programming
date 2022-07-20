cea155601c518b57dc787728e87a4086a2e6a49d
muadh haji <muadhhaji320@gmail.com>
2022-07-20 09:58:45 -0700

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _print_rev_recursion - prints a given string in reverse
- *@s: given parameter
- *@Return: void
- */
+* _print_rev_recursion - prints a given string in reverse
+*@s: given parameter
+*@Return: void
+*/
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
