c43b102c80ea5e16b97f0f9ab1d2b16b9e0695bc
muadh haji <muadhhaji320@gmail.com>
2022-07-20 09:46:20 -0700

    task0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,18 @@
 *char prototypes(char)
 */
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _putchar(char c);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int get_strlen(char *s);
+int find_palindrome(char *s, int len, int index);
+int wildcmp(char *s1, char *s2);
+int _sqrt(int n, int x);
+int is_prime(int n, int start);
 
 #endif /* MAIN_H */
