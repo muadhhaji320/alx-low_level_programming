3ae74ec2abdd41e2439c1f410946d0640382ed24
muadh haji <muadhhaji320@gmail.com>
2022-07-28 08:24:14 -0700

    hot fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,7 +12,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *iterate_zeroes(char *str);
 int len(char *str);
 char *create_xarray(int size);
 int get_digit(char c);
